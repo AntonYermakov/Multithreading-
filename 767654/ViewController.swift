ff06ef289ab8f0b3a9889d5e07531db691fd1651
AntonYermakov <yermakov7anton@gmail.com>
2017-04-27 13:00:59 +0300

    add new project

---
+++ 767654/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  767654
+//
+//  Created by Yermakov Anton on 27.04.17.
+//  Copyright © 2017 Yermakov Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+    @IBAction func button1Pressed(_ sender: UIButton) {
+        dowloadImage(button: 1)
+    }
+    
+    @IBAction func button2Pressed(_ sender: UIButton) {
+        dowloadImage(button: 2)
+    }
+    
+    @IBAction func button3Pressed(_ sender: UIButton) {
+        dowloadImage(button: 3)
+    }
+    
+    func dowloadImage(button: Int){
+        
+        var url = URL(string: "")
+        
+        switch button {
+        case 1: url = URL(string: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSgc_VD_ytllzp_DzqeYPS0CAFxGjwFpYKw_4ngm7q8SZqJJK_r")
+        case 2: url = URL(string: "http://www.threepullpa.com/data/uploads/36/405317-images-of-nature-amazing-place.jpg")
+        case 3: url = URL(string: "http://wallpaperpulse.com/img/1010484.jpg")
+        default: break
+        }
+        
+        let fetch = NSData(contentsOf: url! as URL)
+        if let dataImage = fetch {
+            self.myImage.image = UIImage(data: dataImage as Data)
+        }
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
